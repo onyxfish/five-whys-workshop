c10d2449c6e79d4e111accce5eb974b478eaced6
Christopher Groskopf <chrisgroskopf@gmail.com>
2021-01-07 07:41:54 -0800

    Add header underline.

--- example.js
+++ example.js
@@ -7,12 +7,16 @@ function doubleCheck (h, r) {
 function makeTable (h = [], d = [], capitalize = false) {
   const s = '|';
   let t = '|';
+  let under = '';
 
   for (const header in h) {
     const h2 = capitalize ? h[header].charAt(0).toUpperCase() + h[header].slice(1) : h[header];
 
     t += ' ' + h2 + ' ' + s;
+    under += ' ' + '-'.repeat(h2.length) + ' ' + s;
   }
+
+  t += '\n' + s + under;
 
   for (let i = 0; i < d.length; i++) {
     t += '\n' + s;
--- example.test.js
+++ example.test.js
@@ -8,6 +8,7 @@ test('Prints a simple table', () => {
     )
   ).toBe(
 `| first | second | third |
+| ----- | ------ | ----- |
 | 1 | 2 | 3 |
 | 4 | 5 | 6 |`
   );
@@ -22,6 +23,7 @@ test('Capitalize headers', () => {
     )
   ).toBe(
 `| First | Second | Third |
+| ----- | ------ | ----- |
 | 1 | 2 | 3 |
 | 4 | 5 | 6 |`
   );
